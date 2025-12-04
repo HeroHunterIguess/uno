6ada0e439b44fca80562a6617947b5224ac68ef2
HeroHunter <abramscarter1999@gmail.com>
2025-12-03 19:32:05 -0500

    formatting changes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-/*--------------------------------------------------*\
+/*------------------------------------------------*\
 : uno-like game
 : created by: Hero
 : 
-: some of this code prob isnt amazing im bad at rust
-\*--------------------------------------------------*/
-
+: some of this code prob isnt good im bad at rust 
+\*------------------------------------------------*/
 
 use rand::Rng;
 
@@ -12,15 +11,12 @@ use rand::Rng;
 const COLOR_OPTIONS: [char; 4] = ['r','y','b','g'];
 const NUMBER_OPTIONS: [char; 9] = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-
 /*------------------------------*\
 : defining essential functions   
 \*------------------------------*/
 
-
 //pulling a single card
 fn pull_card() -> String {
-
     //initialize rng and list the card gets made in
     let mut rng = rand::thread_rng();
     let mut card_generating_list: Vec<char> = Vec::new();
@@ -37,26 +33,20 @@ fn pull_card() -> String {
 
 //generating a deck
 fn generate_deck() -> Vec<String> {
-
-    //initializing deck
+    //initializing and creating deck
     let mut deck: Vec<String> = Vec::new();
-
     while deck.len() < 7 {
         //adding a single card
         deck.push(pull_card());
-
     }
     return deck;
 }
-
 
 /*------------------------------*\
 : starting main game
 \*------------------------------*/
 
-
 fn main() {
-
     //initializating both players decks
     let player1_deck = generate_deck();
     let player2_deck = generate_deck();
@@ -64,11 +54,8 @@ fn main() {
     println!("player 1s deck: {:?}",player1_deck);
     println!("player 2s deck: {:?}",player2_deck);
 
-
     //setup the stack of cards with a random card on top
     let card_on_stack = pull_card();
     println!("{card_on_stack}");
 
-
 }
-
